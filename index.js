74fcedac876e2767179cd79c6b6c4b58783178c3
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-06-09 16:23:49 +0600

    create jwt api and verifyJWT function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
@@ -10,6 +11,22 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+// verify jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 
 // const uri = 'mongodb://localhost:27017'
@@ -36,13 +53,32 @@ async function run() {
     const reviewCollection = client.db("languageDB").collection("reviews")
 
 
+    // jwt api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
     // classes related apis
+    app.get('/classes', async(req, res) => {
+        const result = await classCollection.find().toArray();
+        res.send(result)
+    })
+
     app.get('/popular-classes', async(req, res) => {
         const result = await classCollection.find().sort({ students: -1 }).limit(6).toArray();
         res.send(result)
     })
 
     // instructors related apis
+    app.get('/instructors', async(req, res) => {
+      const result = await instructorCollection.find().toArray()
+      res.send(result)
+    })
+
     app.get('/popular-instructors', async(req, res) => {
       const pipeline = [
         {
@@ -79,10 +115,6 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/instructors', async(req, res) => {
-      const result = await instructorCollection.find().toArray()
-      res.send(result)
-    })
 
     // review api
     app.get('/reviews', async(req, res) => {
